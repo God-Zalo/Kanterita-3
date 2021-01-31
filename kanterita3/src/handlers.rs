f1f523c48ac4e45e48ba50efc18f07ee8edccdec
God-Zalo <martinez.zalocin@gmail.com>
2021-01-31 17:31:35 -0500

    Feature: - Added upload logic*, with multipart dependencies - fn main creates folder to hold uploaded files - .gitignore ignores everything under ./tmp
    * this could panic anytime as no error handling is implemented
    solves #3

--- kanterita3/src/handlers.rs
+++ kanterita3/src/handlers.rs
@@ -1,12 +1,56 @@
-use crate::models::{Status, CreatePerson};
+//multipart
+use std::io::Write;
+use actix_multipart::Multipart;
+use futures::{StreamExt, TryStreamExt};
+
+//everything else
+use crate::models::{Status,CreatePerson};
 use crate::db;
 use deadpool_postgres::{Pool, Client};
-use actix_web::{web, Responder, HttpResponse};
+use actix_web::{web, Error, Responder, HttpResponse};
 
 
 pub async fn status() -> impl Responder {
     web::HttpResponse::Ok()
         .json(Status { status: "Ok".to_string() })
+}
+
+
+pub async fn save_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
+    // iterate over multipart stream
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        let content_type = field.content_disposition().unwrap();
+        let filename = content_type.get_filename().unwrap();
+        let filepath = format!("./tmp/{}", sanitize_filename::sanitize(&filename));
+
+        // File::create is blocking operation, use threadpool
+        let mut f = web::block(|| std::fs::File::create(filepath))
+            .await
+            .unwrap();
+
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.next().await {
+            let data = chunk.unwrap();
+            // filesystem operations are blocking, we have to use threadpool
+            f = web::block(move || f.write_all(&data).map(|_| f)).await?;
+        }
+    }
+    Ok(web::HttpResponse::Ok().json(Status { status: "Ok".to_string()}))
+}
+
+
+pub async fn index() -> HttpResponse {
+    let html = r#"<html>
+        <head><title>Upload Test</title></head>
+        <body>
+            <form target="/" method="post" enctype="multipart/form-data" accept-charset=utf-8 >
+                <input type="file" multiple name="file"/>
+                <button type="submit">Submit</button>
+            </form>
+        </body>
+    </html>"#;
+
+    HttpResponse::Ok().body(html)
 }
 
 pub async fn get_people(db_pool: web::Data<Pool>) -> impl Responder{
